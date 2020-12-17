07fb54b3e60a94e7cf8399d34fb7e626c953cb31
jaykukreja28 <42570780+jaykukreja28@users.noreply.github.com>
2020-12-17 13:40:21 +0530

    first commit
    Lunch Break pls come early also add two numbers

---
+++ LunchBreak.java
@@ -0,0 +1,9 @@
+class LunchBreak{
+  public static int add(int num1 , int num2){
+      return num1+num2;    
+  } 
+  public static void main(){
+      System.out.println(add(10,20));  
+  }  
+  
+}
