643a3337ec5f380410ab88573d3f5182c6dcba7f
jaykukreja28 <42570780+jaykukreja28@users.noreply.github.com>
2020-12-18 15:54:15 +0530

    change - add sub method

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -4,4 +4,7 @@ public class Calc {
 		public void add(int n1, int n2){
 			System.out.println("Sum = "+ (n1+n2));
 		}
+	public void sub(int n1, int n2){
+			System.out.println("Sub = "+ (n1-n2));
+		}
 }
