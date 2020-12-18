4157b0383840d3ee705c2e8dbe44f8842a6ee103
jaykukreja28 <jaykukreja28@gmail.com>
2020-12-18 13:42:04 +0530

    Added method for division for Calc classssssssssss

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -10,4 +10,7 @@ public class Calc {
 		public void mul(int n1, int n2){
 			System.out.println("Multiplication = "+ (n1*n2));
 		}
+		public void jajantaramMamantaram(int n1, int n2){
+			System.out.println("Division = "+ (n1/n2));
+		}
 }
